ebe4a3b89a7d1f96a3e5dae5e02993500557db01
Saji732 <jcgsaji@gmail.com>
2023-11-07 01:24:56 +0530

    king prject added

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import Signup from './Pages/Signup';
+import Login from "./Pages/Login";
+import Home from "./Pages/Home"
+import {Route, Routes} from "react-router-dom"
+
+function App() {
+  
+
+  return (
+    <>
+    
+       {/* <Login/>
+    <Signup/> */}
+    <Routes>
+      <Route path="/" element={<Signup/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path='/home' element={<Home/>}/>
+    </Routes>
+    
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Styles/Home.css"
+import { Container } from 'react-bootstrap'
+
+const Home = () => {
+  return (
+    <Container>
+        <h>HOTEL KING PALACE</h>
+        <p>Welcome</p>
+        <button varient="primary" type="submit">Booking</button>
+    </Container>
+  )
+}
+
+export default Home
